c3081355ecf6ba69bd4d8efe1f4b591995326e1a
Mohnish Devadiga <mohnish226@yahoo.co.in>
2017-12-31 19:34:39 +0530

    Regular Expression

--- Regular_Expression.py
+++ Regular_Expression.py
@@ -9,28 +9,25 @@ Created on Sat Dec 30 21:10:38 2017
 import nltk
 import re
 
+def print_word(re_word):
+    print("\n Printing")
+    for item in re_word:
+        print(item)
+
 alice = nltk.corpus.gutenberg.words("carroll-alice.txt")
 
 #Words that start with "new"
 alice_new = set([word for word in alice if re.search("^new", word)])
-
-for item in alice_new:
-    print(item)
+print_word(alice_new)
     
 #Ends with "ful"
 alice_ful = set([word for word in alice if re.search("ful$",word)])
-
-for item in alice_ful:
-    print(item)
+print_word(alice_ful)
     
 #Contains "nn"
 alice_nn = set([word for word in alice if re.search("^.*nn.*$",word)])
-
-for item in alice_nn:
-    print(item)
+print_word(alice_nn)
     
 #Starts with a vowel
 alice_aeiou = set([word for word in alice if re.search("^[aeiou].+$",word)])
-
-for item in alice_aeiou:
-    print(item)
+print_word(alice_aeiou)
