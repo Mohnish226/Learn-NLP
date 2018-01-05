666cb28f00ea0d359e4c3ed2d4149f67247233e2
Mohnish Devadiga <mohnish226@yahoo.co.in>
2018-01-05 23:35:43 +0530

    Named Entity

---
+++ named_entity.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan  5 20:47:48 2018
+
+@author: Mohnish_Devadiga
+"""
+
+import nltk
+
+text = open("data/example.txt").read()
+
+text_tag = nltk.pos_tag(nltk.word_tokenize(text))
+
+text_ch = nltk.ne_chunk(text_tag)
+
+for chunk in text_ch:
+    if hasattr(chunk, 'label'):
+        print(chunk.label()," ".join(c[0] for c in chunk.leaves()))
